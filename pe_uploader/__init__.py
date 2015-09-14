54d1771c8d791adb00233c16ec389b1f5e126124
Ryosuke Sato <rskjtwp@gmail.com>
2015-09-14 21:58:44 +0900

    アップロードができるようになった

--- pe_uploader/__init__.py
+++ pe_uploader/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
+
+UPLOAD_FOLDER = os.path.dirname(__file__) + "/files"
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.debug = True
 app.config.from_object('pe_uploader.config')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
 db = SQLAlchemy(app)
 
--- pe_uploader/views.py
+++ pe_uploader/views.py
@@ -1,9 +1,48 @@
-from flask import request, redirect, url_for, render_template, flash
+import os
+from flask import request, redirect, url_for, render_template, \
+    send_from_directory
 from pe_uploader import app, db
 from pe_uploader.models import Files
+from werkzeug import secure_filename
 
 
 @app.route('/')
 def list_files():
     files = Files.query.order_by(Files.id.desc()).all()
     return render_template('list_files.html', files=files)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        fileobj = request.files['file']
+        path = "{upload}/{name}".format(
+            upload=app.config['UPLOAD_FOLDER'], name=fileobj.filename)
+        if fileobj and allowed_file(fileobj.filename):
+            filename = secure_filename(fileobj.filename)
+            fileobj.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # Register db
+            file = Files(name=fileobj.filename, path=path)
+            print(file)
+            db.session.add(file)
+            db.session.commit()
+            # return redirect(url_for('uploaded_file', filename=filename))
+        return redirect(url_for('list_files'))
+    return render_template('upload.html')
+
+
+# 実際にファイルをアップロードするところ
+@app.route('/upload/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+# 各ファイルの情報を見る
+@app.route('/files/<int:file_id>/')
+def show_files(file_id):
+    return NotImplementedError()

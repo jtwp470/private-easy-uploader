47f2858d19a53f0e063210a12e17796c5a1df48a
Ryosuke Sato <rskjtwp@gmail.com>
2015-09-30 23:11:14 +0900

    Add login & logout function
    Signed-off-by: Ryosuke Sato <rskjtwp@gmail.com>

--- pe_uploader/views.py
+++ pe_uploader/views.py
@@ -1,9 +1,29 @@
 import os
+from functools import wraps
+
 from flask import request, redirect, url_for, render_template, \
-    send_from_directory, flash, jsonify
+    send_from_directory, flash, jsonify, session, g
 from pe_uploader import app, db
-from pe_uploader.models import Files
+from pe_uploader.models import Files, User
 from werkzeug import secure_filename
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.path))
+        return f(*args, **kwargs)
+    return decorated_view
+
+
+@app.before_request
+def load_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(session['user_id'])
 
 
 @app.route('/')
@@ -31,26 +51,31 @@ def hashed_filename(filename):
 
 
 @app.route('/upload', methods=['GET', 'POST'])
+@login_required
 def upload_file():
     if request.method == 'POST':
         fileobj = request.files['file']
-        if fileobj and allowed_file(fileobj.filename):
-            filename_raw = secure_filename(fileobj.filename)  # 生のファイル名
-            filename_hashed = hashed_filename(filename_raw)   # ハッシュ化されたファイル名
-            # 保存先はハッシュ化されたファイルパスを選択する必要がある
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'],
-                                    filename_hashed)
-            fileobj.save(filepath)
-            # Register db
-            file = Files(name=filename_raw, hashed=filename_hashed,
-                         path="files/" + filename_hashed)
-            db.session.add(file)
-            db.session.commit()
-            flash('<div class="alert alert-success" role="alert">Successfly upload file</div>')
-            # return redirect(url_for('uploaded_file', filename=filename))
-            return redirect(url_for('list_files'))
+        if fileobj:
+            if allowed_file(fileobj.filename):
+                filename_raw = secure_filename(fileobj.filename)  # 生のファイル名
+                filename_hashed = hashed_filename(filename_raw)   # ハッシュ化されたファイル名
+                # 保存先はハッシュ化されたファイルパスを選択する必要がある
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'],
+                                        filename_hashed)
+                fileobj.save(filepath)
+                # Register db
+                file = Files(name=filename_raw, hashed=filename_hashed,
+                             path="files/" + filename_hashed)
+                db.session.add(file)
+                db.session.commit()
+                flash('<div class="alert alert-success" role="alert">Successfly upload file</div>')
+                # return redirect(url_for('uploaded_file', filename=filename))
+                return redirect(url_for('list_files'))
+            else:
+                flash('<div class="alert alert-danger" role="alert">Your file type is NOT allowd.</div>')
+                return redirect(url_for('upload_file'))
         flash('<div class="alert alert-info" role="alert">No file uploaded.</div>')
-        return redirect(url_for('list_files'))
+        return redirect(url_for('upload_file'))
     return render_template('upload.html')
 
 
@@ -62,6 +87,7 @@ def uploaded_file(filename):
 
 # ファイルを削除する
 @app.route('/files/<filehashed>/delete', methods=['DELETE'])
+@login_required
 def delete_file(filehashed):
     file = Files.query.filter(Files.hashed == filehashed).first()
     if file is None:
@@ -72,3 +98,62 @@ def delete_file(filehashed):
     db.session.delete(file)
     db.session.commit()
     return jsonify({'status': 'OK'})
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user, authenticated = User.authenticate(db.session.query,
+                                                request.form['name'],
+                                                request.form['password'])
+        if authenticated:
+            session['user_id'] = user.id
+            # session['nonce'] =
+            # TODO Prevent CSRF
+            flash('<div class="alert alert-success" role="alert">You were logged in</div>')
+            return redirect(url_for('list_files'))
+        else:
+            flash('<div class="alert alert-danger" role="alert">Invalid user or password</div>')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('<div class="alert alert-success" role="alert">You were logged out</div>')
+    return redirect(url_for('list_files'))
+
+
+@app.route('/admin/add', methods=["GET", "POST"])
+def add_admin():
+    admin = User.query.get(1)
+    if admin is None:
+        # Adminを作成する
+        if request.method == 'POST':
+            admin = User(name=request.form['name'],
+                         password=request.form['password'])
+            db.session.add(admin)
+            db.session.commit()
+            flash('<div class="alert alert-success" role="alert">Successfly add your admin user</div>')
+            return redirect(url_for('login'))
+        else:
+            return render_template('admin/add.html')
+    return redirect(url_for('edit_admin'))
+
+
+# パスワードとユーザー名を変更する
+@app.route('/admin/edit', methods=["GET", "POST"])
+@login_required
+def edit_admin():
+    admin = User.query.get(1)
+    if admin is None:
+        return redirect(url_for('add_admin'))
+    if request.method == 'POST':
+        if request.form['name']:
+            admin.name = request.form['name']
+        if request.form['password']:
+            admin.password = request.form['password']
+        db.session.commit()
+        flash('<div class="alert alert-success" role="alert">Successfly change your password</div>')
+        return redirect(url_for('logout'))
+    return render_template('admin/edit.html', user=admin)
